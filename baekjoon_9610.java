6e02576409293feffcbbb03a7fc4e42f28dabef4
fud3725 <34434153+dailyHR@users.noreply.github.com>
2021-10-21 23:50:06 +0900

    백준 9610: 사분면
    다시 열심히 ~_~

---
+++ baekjoon_9610.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 백준 9610번: 사분면
+ * */
+
+public class baekjoon_9610 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int q1= 0;
+		int q2 = 0;
+		int q3= 0;
+		int q4= 0;
+		int axis = 0;
+
+		for(int i = 0 ; i < N ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			if(x > 0 && y >0) {
+				q1++;
+			}else if(x < 0 && y >0) {
+				q2++;
+			}else if(x < 0 && y < 0) {
+				q3++;
+			}else if(x > 0 && y <0) {
+				q4++;
+			}
+			else {
+				axis++;
+			}
+			
+		}
+		System.out.println("Q1: " + q1);
+		System.out.println("Q2: " + q2);
+		System.out.println("Q3: " + q3);
+		System.out.println("Q4: " + q4);
+		System.out.println("AXIS: " + axis);
+
+
+	}
+	
+}
