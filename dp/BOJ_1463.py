5823cbd8c57eb153cdc033483f722ac71a7d9f6e
fud3725 <34434153+dailyHR@users.noreply.github.com>
2022-02-08 22:02:59 +0900

    1로 만들기(2,3,-1)
    복습용

---
+++ dp/BOJ_1463.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 21:54:31 2022
+
+@author: fud37
+"""
+
+X = int(input())
+dp =[0] * (X+1)
+
+for i in range(2, X+1):
+    dp[i] = dp[i-1] + 1
+    
+    if i % 3 == 0 :
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    
+    if i % 2 ==0 :
+        dp[i] = min(dp[i], dp[i//2] + 1)
+        
+print(dp[X])
+
