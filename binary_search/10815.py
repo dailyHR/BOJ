05d5f7bbbf49645e2cc309f7d3c0de6f33551c26
fud3725 <34434153+ryeong904@users.noreply.github.com>
2022-03-15 21:40:50 +0900

    [BOJ-10815] 숫자 카드
    이진 탐색

---
+++ binary_search/10815.py
@@ -0,0 +1,20 @@
+def binary_search(arr, target, start, end):
+    if start > end:
+        return 0
+    mid = (start + end)//2
+    if arr[mid] == target :
+        return 1
+    elif arr[mid] > target :
+        return binary_search(arr, target, start, mid-1)
+    else :
+        return binary_search(arr, target, mid+1, end)
+
+n = int(input())
+arr1 = list(map(int, input().split()))
+arr1.sort()
+
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for x in arr2 :
+    print(binary_search(arr1, x, 0, n-1), end=' ')
