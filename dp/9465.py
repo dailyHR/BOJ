ae24ec8edea18007b44d7bb6beeaec8c3cffce34
Hye Ryeong <34434153+ryeong904@users.noreply.github.com>
2022-05-06 21:31:53 +0900

    [BOJ-9465] 스티커
    수정 필요

---
+++ dp/9465.py
@@ -0,0 +1,26 @@
+T = int(input())
+# 이전 값을 선택하거나 현재 값을 선택함
+for _ in range(T):
+    N = int(input())
+    sticker = []
+    dp = [[0] * N for _ in range(2)]
+    for _ in range(2):
+        sticker.append(list(map(int, input().split())))
+
+    dp[0][0] = sticker[0][0]
+    dp[1][0] = sticker[1][0]
+
+    dp[0][1] = dp[1][0] + sticker[0][1]
+    dp[1][1] = dp[0][0] + sticker[1][1]
+
+    for i in range(2,N):
+        dp[0][i] = max(
+            max(dp[0][i-2], dp[1][i-2]) + sticker[0][i],
+            dp[1][i-1] + sticker[0][i]
+        )
+
+        dp[1][i] = max(
+            max(dp[0][i-2], dp[1][i-2]) + sticker[1][i],
+            dp[0][i-1] + sticker[0][i]
+        )
+    print(max(max(dp[0]), max(dp[1])))
