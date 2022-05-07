d81fc8fdde1ed1decfff0b807356a66d06dd97d6
Hye Ryeong <34434153+ryeong904@users.noreply.github.com>
2022-05-07 17:53:15 +0900

    [BOJ-9465] 스티커
    DP
    N이 1일 경우 코드 추가, N이 1보다 클 때의 코드에 있던 오타 수정

--- dp/9465.py
+++ dp/9465.py
@@ -2,25 +2,34 @@ T = int(input())
 # 이전 값을 선택하거나 현재 값을 선택함
 for _ in range(T):
     N = int(input())
-    sticker = []
-    dp = [[0] * N for _ in range(2)]
-    for _ in range(2):
-        sticker.append(list(map(int, input().split())))
+    
+    if N == 1 :
+        n1 = int(input())
+        n2 = int(input())
+        if n1 > n2 :
+            print(n1)
+        else: 
+            print(n2)
+    else:
+        sticker = []
+        dp = [[0] * N for _ in range(2)]
+        for _ in range(2):
+            sticker.append(list(map(int, input().split())))
 
-    dp[0][0] = sticker[0][0]
-    dp[1][0] = sticker[1][0]
+        dp[0][0] = sticker[0][0]
+        dp[1][0] = sticker[1][0]
 
-    dp[0][1] = dp[1][0] + sticker[0][1]
-    dp[1][1] = dp[0][0] + sticker[1][1]
+        dp[0][1] = dp[1][0] + sticker[0][1]
+        dp[1][1] = dp[0][0] + sticker[1][1]
 
-    for i in range(2,N):
-        dp[0][i] = max(
-            max(dp[0][i-2], dp[1][i-2]) + sticker[0][i],
-            dp[1][i-1] + sticker[0][i]
-        )
+        for i in range(2,N):
+            dp[0][i] = max(
+                max(dp[0][i-2], dp[1][i-2]) + sticker[0][i],
+                dp[1][i-1] + sticker[0][i]
+            )
 
-        dp[1][i] = max(
-            max(dp[0][i-2], dp[1][i-2]) + sticker[1][i],
-            dp[0][i-1] + sticker[0][i]
-        )
-    print(max(max(dp[0]), max(dp[1])))
+            dp[1][i] = max(
+                max(dp[0][i-2], dp[1][i-2]) + sticker[1][i],
+                dp[0][i-1] + sticker[1][i]
+            )
+        print(max(max(dp[0]), max(dp[1])))
