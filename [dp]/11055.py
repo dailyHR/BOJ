7a0954cfb3435fc19d64a494a1cccb2235f865d2
Hye Ryeong <34434153+ryeong904@users.noreply.github.com>
2022-05-13 21:40:43 +0900

    [BOJ-11055] 가장 큰 증가 부분 수열
    DP

--- [dp]/11055.py
+++ [dp]/11055.py
@@ -1,13 +1,12 @@
 n = int(input())
 a = list(map(int, input().split()))
-dp = [1] * n
+dp = a[:]
 
 value = 0
 for i in range(n):
     for j in range(i):
         if a[j] < a[i]:
-            if dp[j] + 1 > dp[i]:
-                dp[i] = dp[j] + 1
+            dp[i] = max(dp[i], a[i] + dp[j])
 
 
-print(value)
+print(max(dp))
